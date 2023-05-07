4f67553055223e0665abadf0b28a5c5c17baadc9
jmcb <joelsgp@protonmail.com>
2023-05-07 15:15:42 +0100

    Finally concede to lukas on quote style

--- uoy_assessment_uploader/__init__.py
+++ uoy_assessment_uploader/__init__.py
@@ -12,35 +12,35 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 TIMEOUT = 10
-URL_BASE = 'https://teaching.cs.york.ac.uk/'
-URL_SUBMIT = 'https://teaching.cs.york.ac.uk/student/2021-2/submit/COM00012C/901/A'
-URL_LOGIN = 'https://shib.york.ac.uk/idp/profile/SAML2/Redirect/SSO?execution=e1s1'
-URL_EXAM_NUMBER = 'https://teaching.cs.york.ac.uk/student/confirm-exam-number'
+URL_BASE = "https://teaching.cs.york.ac.uk/"
+URL_SUBMIT = "https://teaching.cs.york.ac.uk/student/2021-2/submit/COM00012C/901/A"
+URL_LOGIN = "https://shib.york.ac.uk/idp/profile/SAML2/Redirect/SSO?execution=e1s1"
+URL_EXAM_NUMBER = "https://teaching.cs.york.ac.uk/student/confirm-exam-number"
 
 
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser()
 
-    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument("-u", "--username", required=True)
     parser.add_argument(
-        '--password',
-        help='Not recommended to pass this as an argument, for security reasons.'
-        ' Leave it out and you will be securely prompted to enter it at startup.',
+        "--password",
+        help="Not recommended to pass this as an argument, for security reasons."
+        " Leave it out and you will be securely prompted to enter it at startup.",
     )
-    parser.add_argument('-e', '--exam-number', required=True)
-    parser.add_argument('-f', '--file', type=Path, default='exam.zip')
-    parser.add_argument('--cookie-file', type=Path, default='.cookies.json')
+    parser.add_argument("-e", "--exam-number", required=True)
+    parser.add_argument("-f", "--file", type=Path, default="exam.zip")
+    parser.add_argument("--cookie-file", type=Path, default=".cookies.json")
     parser.add_argument(
-        '--no-save-cookies', dest='do_save_cookies', action='store_false'
+        "--no-save-cookies", dest="do_save_cookies", action="store_false"
     )
-    parser.add_argument('-q', '--headless', action='store_true')
+    parser.add_argument("-q", "--headless", action="store_true")
 
     return parser
 
 
 def save_cookies(driver: WebDriver, fp: Path):
     cookies = driver.get_cookies()
-    with open(fp, 'w') as f:
+    with open(fp, "w") as f:
         json.dump(cookies, f)
 
 
@@ -51,63 +51,61 @@ def load_cookies(driver: webdriver.Chrome, fp: Path):
     except FileNotFoundError:
         print("Not loading cookies, file doesn't exist")
     else:
-        print('Loading cookies')
+        print("Loading cookies")
         for c in cookies:
-            driver.execute_cdp_cmd('Network.setCookie', c)
+            driver.execute_cdp_cmd("Network.setCookie", c)
 
 
 def login(driver: WebDriver, username: str, password: str):
-    input_username = driver.find_element(By.ID, 'username')
+    input_username = driver.find_element(By.ID, "username")
     input_username.send_keys(username)
-    input_password = driver.find_element(By.ID, 'password')
+    input_password = driver.find_element(By.ID, "password")
     input_password.send_keys(password)
-    input_button = driver.find_element(By.NAME, '_eventId_proceed')
+    input_button = driver.find_element(By.NAME, "_eventId_proceed")
     input_button.click()
 
 
 def enter_exam_number(driver: WebDriver, exam_number: str):
-    input_exam_number = driver.find_element(By.ID, 'examNumber')
+    input_exam_number = driver.find_element(By.ID, "examNumber")
     input_exam_number.send_keys(exam_number)
     input_exam_number.submit()
 
 
 def upload(driver: WebDriver, fp: Path):
-    input_file = driver.find_element(By.ID, 'file')
+    input_file = driver.find_element(By.ID, "file")
     input_file.send_keys(str(fp.resolve()))
-    input_checkbox = driver.find_element(By.ID, 'ownwork')
+    input_checkbox = driver.find_element(By.ID, "ownwork")
     input_checkbox.click()
     input_checkbox.submit()
 
 
-def do(
-    driver: WebDriver, username: str, password: str, exam_number: str, fp: Path
-):
+def do(driver: WebDriver, username: str, password: str, exam_number: str, fp: Path):
     wait = WebDriverWait(driver, TIMEOUT)
 
     while True:
         driver.get(URL_SUBMIT)
         if driver.current_url == URL_LOGIN:
-            print('Logging in..')
+            print("Logging in..")
             login(driver, username, password)
             wait.until(
                 ec.any_of(ec.url_to_be(URL_EXAM_NUMBER), ec.url_to_be(URL_SUBMIT))
             )
         elif driver.current_url == URL_EXAM_NUMBER:
-            print('Entering exam number..')
+            print("Entering exam number..")
             enter_exam_number(driver, exam_number)
             wait.until(ec.url_to_be(URL_SUBMIT))
         elif driver.current_url == URL_SUBMIT:
-            print('Uploading file...')
+            print("Uploading file...")
             upload(driver, fp)
             wait.until(
                 ec.text_to_be_present_in_element(
-                    [By.CLASS_NAME, 'alert-success'], 'File submitted successfully.'
+                    [By.CLASS_NAME, "alert-success"], "File submitted successfully."
                 )
             )
-            print('Uploaded successfully.')
+            print("Uploaded successfully.")
             break
         else:
-            raise Exception('bruh')
+            raise Exception("bruh")
 
 
 def main():
@@ -124,7 +122,7 @@ def main():
 
     options = webdriver.ChromeOptions()
     if headless:
-        options.add_argument('--headless')
+        options.add_argument("--headless")
 
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(TIMEOUT)
@@ -137,12 +135,12 @@ def main():
         raise
     else:
         if do_save_cookies:
-            print('Saving cookies')
+            print("Saving cookies")
             save_cookies(driver, cookie_path)
 
-    input('Press ENTER to close driver:\n')
+    input("Press ENTER to close driver:\n")
     driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
