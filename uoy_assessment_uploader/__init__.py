992806e2ce115522ab0c80aee5a98e7655116984
jmcb <joelsgp@protonmail.com>
2023-05-07 17:36:02 +0100

    New options to dry run or delete cookies

--- uoy_assessment_uploader/__init__.py
+++ uoy_assessment_uploader/__init__.py
@@ -14,12 +14,17 @@ from selenium.webdriver.support import expected_conditions as ec
 
 
 # todo: re-implement with saml auth and requests, as alternative to selenium
+# todo readme
 
 
 __version__ = "0.1.0"
 
 # timeout for selenium waits, in seconds
 TIMEOUT = 10
+
+DEFAULT_ARG_FILE = "exam.zip"
+DEFAULT_ARG_COOKIE_FILE = ".cookies.json"
+
 URL_SUBMIT_BASE = "https://teaching.cs.york.ac.uk/student"
 URL_LOGIN = "https://shib.york.ac.uk/idp/profile/SAML2/Redirect/SSO?execution=e1s1"
 URL_EXAM_NUMBER = "https://teaching.cs.york.ac.uk/student/confirm-exam-number"
@@ -28,6 +33,7 @@ URL_EXAM_NUMBER = "https://teaching.cs.york.ac.uk/student/confirm-exam-number"
 def get_parser() -> ArgumentParser:
     parser = ArgumentParser(description=__doc__)
 
+    # core functionality arguments
     parser.add_argument("-u", "--username", required=True)
     parser.add_argument(
         "--password",
@@ -35,16 +41,28 @@ def get_parser() -> ArgumentParser:
         " Leave it out and you will be securely prompted to enter it at startup.",
     )
     parser.add_argument("-e", "--exam-number", required=True)
-    parser.add_argument("-f", "--file", type=Path, default="exam.zip")
+    parser.add_argument("-f", "--file", type=Path, default=DEFAULT_ARG_FILE)
     parser.add_argument(
         "-n",
         "--submit-url",
         required=True,
         help="The specific exam to upload to, e.g. /2021-2/submit/COM00012C/901/A",
     )
-    parser.add_argument("--cookie-file", type=Path, default=".cookies.json")
+    # options
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Log in but don't actually upload the file.",
+    )
+    # selenium related
+    parser.add_argument("--cookie-file", type=Path, default=DEFAULT_ARG_COOKIE_FILE)
     parser.add_argument(
         "--no-save-cookies", dest="do_save_cookies", action="store_false"
+    )
+    parser.add_argument(
+        "--delete-cookies",
+        action="store_true",
+        help="Before starting, delete previous login cookies (if they exist).",
     )
     parser.add_argument("-q", "--headless", action="store_true")
 
@@ -84,12 +102,14 @@ def enter_exam_number(driver: WebDriver, exam_number: str):
     input_exam_number.submit()
 
 
-def upload(driver: WebDriver, file_name: str):
+def upload(driver: WebDriver, file_name: str, dry_run: bool):
     input_file = driver.find_element(By.ID, "file")
     input_file.send_keys(file_name)
     input_checkbox = driver.find_element(By.ID, "ownwork")
     input_checkbox.click()
-    input_checkbox.submit()
+    if not dry_run:
+        # todo don't use submit()
+        input_checkbox.submit()
 
 
 def do(
@@ -99,9 +119,11 @@ def do(
     password: str,
     exam_number: str,
     file_name: str,
+    dry_run: bool,
 ):
     wait = WebDriverWait(driver, TIMEOUT)
 
+    # breaks loop on submit
     while True:
         driver.get(submit_url)
         if driver.current_url == URL_LOGIN:
@@ -116,13 +138,16 @@ def do(
             wait.until(ec.url_to_be(submit_url))
         elif driver.current_url == submit_url:
             print("Uploading file...")
-            upload(driver, file_name)
-            wait.until(
-                ec.text_to_be_present_in_element(
-                    [By.CLASS_NAME, "alert-success"], "File submitted successfully."
+            upload(driver, file_name, dry_run)
+            if dry_run:
+                print("Skipped actual upload.")
+            else:
+                wait.until(
+                    ec.text_to_be_present_in_element(
+                        [By.CLASS_NAME, "alert-success"], "File submitted successfully."
+                    )
                 )
-            )
-            print("Uploaded successfully.")
+                print("Uploaded successfully.")
             break
         else:
             raise Exception("bruh")
@@ -145,30 +170,33 @@ def main():
     exam_number: str = args.exam_number
     submit_url: str = resolve_submit_url(args.submit_url)
     fp: Path = args.file
+    dry_run: bool = args.dry_run
     cookie_path: Path = args.cookie_file
     do_save_cookies: bool = args.do_save_cookies
+    delete_cookies: bool = args.delete_cookies
     headless: bool = args.headless
 
     # check zip to be uploaded exists
     if not fp.is_file():
         print(f"File doesn't exist '{fp}'")
         sys.exit(1)
-    fp.resolve()
-    file_name = str(fp)
+    file_name = str(fp.resolve())
 
     # start webdriver
     options = webdriver.ChromeOptions()
     if headless:
         options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(TIMEOUT)
 
-    # load cookies
-    if do_save_cookies:
-        load_cookies(driver, cookie_path)
+    with webdriver.Chrome(options=options) as driver:
+        driver.implicitly_wait(TIMEOUT)
 
-    # run
-    try:
+        # load cookies
+        if delete_cookies:
+            cookie_path.unlink(missing_ok=True)
+        elif do_save_cookies:
+            load_cookies(driver, cookie_path)
+
+        # run
         do(
             driver=driver,
             submit_url=submit_url,
@@ -176,19 +204,13 @@ def main():
             password=password,
             exam_number=exam_number,
             file_name=file_name,
+            dry_run=dry_run,
         )
-    except Exception:
-        raise
-    else:
+
         # save cookies
         if do_save_cookies:
             print("Saving cookies")
             save_cookies(driver, cookie_path)
-
-    # exit
-    # todo context manager
-    input("Press ENTER to close driver:\n")
-    driver.close()
 
 
 if __name__ == "__main__":
