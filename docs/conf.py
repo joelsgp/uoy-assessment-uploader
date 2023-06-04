6fe607085929862c1f5d763f0528a064bce85d3f
jmcb <joelsgp@protonmail.com>
2023-06-04 18:34:25 +0100

    Move sphinx files to docs/

--- docs/conf.py
+++ docs/conf.py
@@ -6,25 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'uoy_assessment_uploader'
-copyright = '2023, jmcb'
-author = 'jmcb'
+project = "uoy_assessment_uploader"
+copyright = "2023, jmcb"
+author = "jmcb"
+release = "0.5.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary"
+    "sphinx.ext.autosummary",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
