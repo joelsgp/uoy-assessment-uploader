bbe6c23b84afbb653e8cf7586f5199fd3dc6ce0f
jmcb <joelsgp@protonmail.com>
2023-06-04 19:23:18 +0100

    Remove unwanted stuff from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,15 +16,14 @@ release = "0.5.2"
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
 ]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# templates_path = ["_templates"]
+# exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+# html_theme = "alabaster"
+# html_static_path = ["_static"]
